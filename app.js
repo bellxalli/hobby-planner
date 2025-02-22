45256c7f9f65b0baf28c16e92c7bc0e23b460a1b
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-02-21 17:49:57 -0800

    Fixed app.js

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+//Import Express
 import express from 'express';
 
-const app = express;
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
 const PORT = 3000;
 
-app.request(express.urlencoded({ extended: true}));
-app.request(express.static('public'));
+const orders = [];
 
+//Define a "default" route for our home page
 app.get('/', (req, res) => {
-     res.render('home');
+
+    // Send our home page as a response to the client
+    res.render('sign-in');
 });
 
 app.listen(PORT, () => {
      console.log(`Server is running at http://localhost:${PORT}`);
-});
+ });
+ 

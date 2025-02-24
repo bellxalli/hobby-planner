274bfebbf7ef2d10974d355a211fe2ffc2ffb092
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-02-24 10:47:29 -0800

    Set up functionality to connect account creation to the list view.

--- app.js
+++ app.js
@@ -11,13 +11,31 @@ app.use(express.static('public'));
 
 const PORT = 3000;
 
-const orders = [];
+const users = [];
 
 //Define a "default" route for our home page
 app.get('/', (req, res) => {
 
     // Send our home page as a response to the client
     res.render('sign-in');
+});
+
+// CHANGE BACK TO "app.post" once connected from "create-account"
+app.get('/account-created', (req, res) => {
+     // Testing verification
+     console.log(req.body);
+
+     const user = {
+          username: req.body.username,
+          password: req.body.password
+     }
+
+     // Add the user to the users database
+     users.push(user);
+     console.log(users);
+
+     // Send "account-created" page
+     res.render('account-created', { user });
 });
 
 // Setting up a route to "list-view" from "sign-in"

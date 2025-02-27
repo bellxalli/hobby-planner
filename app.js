759e3359e9da95a29c0eea5af569000729eec969
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-02-27 13:26:18 -0800

    Added env file and content to app.js

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 //Import Express
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+//Define our database credentials
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.PORT
+});
+
 
 const app = express();
 

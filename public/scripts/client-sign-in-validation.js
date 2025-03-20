b0233525282b18615659105d975c33aa0e5cdb9f
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-03-19 21:01:01 -0700

    added validation for sign in and create account client side

---
+++ public/scripts/client-sign-in-validation.js
@@ -0,0 +1,35 @@
+//Sign in page 
+
+document.getElementById('sing-in').onsubmit = () =>
+{
+    clearErrors();
+    let isValid = true;
+
+    //validate username and password
+
+    let user = document.getElementById('username').value.trim();
+    let pass = document.getElementById('password').value.trim();
+
+    if(user === "")
+    {
+        document.getElementById("err-user").style.display = "block";
+        isValid = false;
+    }
+
+    if(pass === "")
+    {
+        document.getElementById("err-pass").style.display = "block";
+        isValid = true;
+    }
+
+    return isValid;
+}
+
+function clearErrors()
+{
+    let errors = document.getElementsByClassName("err");
+    for(let i = 0;  i < errors.length; i++)
+    {
+        errors[i].style.display = "none";
+    }
+}
